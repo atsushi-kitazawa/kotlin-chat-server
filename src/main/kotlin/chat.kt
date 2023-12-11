18a00adc16f26533dbe978d5a0ddf3187212c46d
atsushi-kitazawa <47269784+atsushi-kitazawa@users.noreply.github.com>
2023-12-11 15:22:24 +0900

    WIP implement chat.kt

--- src/main/kotlin/chat.kt
+++ src/main/kotlin/chat.kt
@@ -4,39 +4,108 @@ import java.net.Socket
 import java.util.Scanner
 import kotlin.concurrent.thread
 
+val server = ChatServer()
 fun main() {
-    ChatServer().init()
+    server.init()
 }
 
 class ChatServer {
-    val PORT: Int = 8000
-    var clientList = mutableListOf<Socket>()
-    var roomList = mutableListOf<Room>()
+    private val PORT: Int = 8000
+    private var roomList = mutableListOf<Room>()
+    private var clientList = mutableListOf<Client>()
 
     fun init() {
         val server = ServerSocket(PORT)
+        initRoom()
         while (true) {
-            val client = server.accept()
-            addClient(client)
-            thread { ClientHandler(client).run() }
+            val clientSocket = server.accept()
+            thread { ClientHandler(clientSocket).run() }
         }
     }
 
-    private fun addClient(client: Socket) {
+    fun login(client: Client) {
+        if (isLogin(client)) {
+            println("client is already logged in")
+            return
+        }
         clientList.add(client)
+        println("login client=${client}")
+        println("DEBUG: clientList=${clientList}")
+    }
+
+    fun logout(client: Client) {
+        if (!isLogin(client)) {
+            println("client is not logged in")
+            return
+        }
+        clientList.remove(client)
+        println("logout client=${client}")
+        println("DEBUG: clientList=${clientList}")
+    }
+
+    fun isLogin(client: Client): Boolean {
+        return clientList.contains(client)
+    }
+
+    fun addRoom(room: Room) {
+        roomList.add(room)
+    }
+
+    fun removeRoom(room: Room) {}
+
+    fun joinedRoom(client: Client, roomName: String) {
+        var room = isExistedRoom(roomName)
+        if (room != null) {
+            room.join(client)
+        }
+    }
+
+    fun leavedRoom(client: Client, roomName: String) {}
+
+    fun isExistedRoom(name: String): Room? {
+        for (n in roomList) {
+            if (n.name == name) {
+                return n
+            }
+        }
+        return null
     }
 
     private fun initRoom() {
-        roomList.add(Room("room1", mutableListOf<Member>()))
-        roomList.add(Room("room2", mutableListOf<Member>()))
+        addRoom(Room("room1"))
+        addRoom(Room("room2"))
     }
-
-    fun addRoom() {}
 }
 
-class Room(val name: String, var members: MutableList<Member>) {}
+data class Room(var name: String) {
+    var joinedClientList = mutableListOf<Client>()
 
-class Member(val name: String, val connection: Socket) {}
+    fun rename(_name: String) {
+        name = _name
+    }
+
+    fun join(client: Client) {
+        joinedClientList.add(client)
+        println("joined room=${name} client=${client}")
+    }
+
+    fun leave(client: Client) {
+        joinedClientList.remove(client)
+        println("leaved room=${name} client=${client}")
+    }
+}
+
+data class Client(var name: String, val connection: Socket) {
+    var joinedRoomList = mutableListOf<Room>()
+
+    fun remave(_name: String) {
+        name = _name
+    }
+
+    fun joinRoom(room: Room) {
+        joinedRoomList.add(room)
+    }
+}
 
 enum class Command {
     login,
@@ -45,27 +114,39 @@ enum class Command {
     leave
 }
 
-class ClientHandler(val client: Socket) {
+class ClientHandler(val cSocket: Socket) {
     fun run() {
-        println("accept ${client}")
-        thread { MessageHandler(client).run() }
+        println("accept ${cSocket}")
+        thread { MessageHandler(cSocket).run() }
     }
 }
 
-class MessageHandler(val client: Socket) {
-    private val reader: Scanner = Scanner(client.getInputStream())
-    private val writer: OutputStream = client.getOutputStream()
+class MessageHandler(val cSocket: Socket) {
+    private val reader: Scanner = Scanner(cSocket.getInputStream())
+    private val writer: OutputStream = cSocket.getOutputStream()
     private var running = true
+    private var client: Client = Client("", cSocket)
     fun run() {
         while (running) {
             val msg = reader.nextLine()
+            if (msg.startsWith(Command.login.name, true)) {
+                val name = msg.substring(Command.join.name.length + 1)
+                client.remave(name)
+                server.login(client)
+                continue
+            }
+            if (msg == Command.logout.name) {
+                server.logout(client)
+                continue
+            }
             if (msg == Command.leave.name) {
                 leave()
                 continue
             }
             if (msg.startsWith(Command.join.name, true)) {
                 val roomName = msg.substring(Command.join.name.length + 1)
-
+                server.joinedRoom(client, roomName)
+                continue
             }
 
             println(msg)
@@ -74,7 +155,6 @@ class MessageHandler(val client: Socket) {
 
     private fun leave() {
         running = false
-        client.close()
-        println("leave ${client}")
+        cSocket.close()
     }
 }
